f85bf595d514cf12bc15633c0084e1b2fd53c0d0
thanhnguyen293 <thanhnguyen02htt@gmail.com>
2023-09-15 12:11:09 +0700

    update getAgentInfo and room.history

--- apps/meteor/app/livechat/server/api/v1/roomHistory.ts
+++ apps/meteor/app/livechat/server/api/v1/roomHistory.ts
@@ -1,10 +1,16 @@
 import { isGETLivechatConfigParams } from '@rocket.chat/rest-typings';
-import { LivechatRooms } from '@rocket.chat/models';
+import { LivechatRooms ,Users} from '@rocket.chat/models';
+import {IOmnichannelRoom} from '@rocket.chat/core-typings';
+
 
 import { API } from '../../../../api/server';
 import { Livechat } from '../../lib/LivechatTyped';
 
 import { callbacks } from '../../../../../lib/callbacks';
+
+function getNameOfUsername(users: Map<string, string>, username: string): string {
+	return users.get(username) || username;
+}
 
 API.v1.addRoute(
 	'livechat/room.history',
@@ -25,11 +31,37 @@ API.v1.addRoute(
 					open: 1,
 					callStatus: 1,
                     lastMessage: 1,
+					unread:1,
 				},
 			};
 
 			const extraQuery = await callbacks.run('livechat.applyRoomRestrictions', {});
-			const rooms = await LivechatRooms.findOpenByVisitorToken(token, options, extraQuery).toArray();
+			 var rooms : IOmnichannelRoom[] = await LivechatRooms.findOpenByVisitorToken(token, options, extraQuery).toArray();
+			const usernames: Set<string> = new Set();
+			rooms.forEach((room) => {
+				if (!room.servedBy.username) {
+					return;
+				}
+				usernames.add(room.servedBy.username);
+			});
+			const names = new Map();
+			(
+				await Users.findUsersByUsernames([...usernames.values()], {
+					projection: {
+						username: 1,
+						name: 1,
+					},
+				}).toArray()
+			).forEach((user) => {
+				names.set(user.username, user.name);
+			});
+
+			rooms.forEach((room: IOmnichannelRoom) => {
+				if (!room.servedBy) {
+					return;
+				}
+				room.servedBy.name = getNameOfUsername(names, room.servedBy.username);
+			});
 
 			return API.v1.success({
 				rooms,
