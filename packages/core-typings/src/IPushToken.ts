8363f0979a2f28360be2e46e7fc522dfbb4974bc
thanhnguyen293 <thanhnguyen02htt@gmail.com>
2023-10-20 17:40:44 +0700

    fix build error

--- packages/core-typings/src/IPushToken.ts
+++ packages/core-typings/src/IPushToken.ts
@@ -1,6 +1,7 @@
 import type { IRocketChatRecord } from './IRocketChatRecord';
 
 export type IPushTokenTypes = 'gcm' | 'apn';
+export type IPushGuestTokenTypes = 'gcm' | 'fcm';
 
 export interface IPushToken extends IRocketChatRecord {
 	token: Record<IPushTokenTypes, string>;
