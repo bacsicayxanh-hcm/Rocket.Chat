1f1116124a926828696b390b3dfc9726b2579921
Thành Nguyễn <thanhnguyen02htt@gmail.com>
2023-11-01 11:41:56 +0700

    disable eventType ROOM_MESSAGE

--- apps/meteor/app/federation/server/endpoints/dispatch.js
+++ apps/meteor/app/federation/server/endpoints/dispatch.js
@@ -15,7 +15,7 @@ import { isFederationEnabled } from '../lib/isFederationEnabled';
 import { getUpload, requestEventsFromLatest } from '../handler';
 import { notifyUsersOnMessage } from '../../../lib/server/lib/notifyUsersOnMessage';
 import { sendAllNotifications } from '../../../lib/server/lib/sendNotificationsOnMessage';
-import { processThreads } from '../../../threads/server/hooks/aftersavemessage';
+// import { processThreads } from '../../../threads/server/hooks/aftersavemessage';
 
 const eventHandlers = {
 	//
