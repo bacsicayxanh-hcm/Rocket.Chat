6ee0c513342c9304782fcc424ff3834dcc9e2483
Thành Nguyễn <thanhnguyen02htt@gmail.com>
2023-12-13 11:46:32 +0700

    fix livechat/agents endpoint missing bio field

--- apps/meteor/server/models/raw/Users.js
+++ apps/meteor/server/models/raw/Users.js
@@ -219,6 +219,7 @@ export class UsersRaw extends BaseRaw {
 					emails: { $first: '$emails' },
 					livechat: { $first: '$livechat' },
 					departments: { $push: '$departments.departmentId' },
+					bio: {$first : '$bio'},
 					customFields:{$first: '$customFields'},
 				},
 			},
